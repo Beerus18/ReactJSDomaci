22be9ef0f59f1c51cf7a15a975b0a42b0c74a43f
Veljko Parezanovic <vparezanovic96@gmail.com>
2021-05-15 13:26:12 +0200

    Finalna verzija ReactJS aplikacije

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<section className="section about-section">
+			<h1 className="section-title">about us</h1>
+			<p>
+				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos
+				deleniti itaque fugit officiis inventore vel nesciunt quaerat
+				voluptatem explicabo cumque ipsam architecto excepturi, rem,
+				incidunt et quas vitae! Cumque eveniet officia porro animi
+				pariatur neque temporibus maxime eaque aut sunt.
+			</p>
+		</section>
+	);
+};
+
+export default About;
